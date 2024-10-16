5b01a6bafb99b6f408fcc1c91d404ea337dd02a0
aroraratta <177751775+aroraratta@users.noreply.github.com>
2024-10-16 05:46:37 +0000

    [Add]部分テンプレートの新規投稿機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
-  
+
   root to: 'homes#top'
   get 'homes/about'
-  
-  get 'users/index'
-  get 'users/show'
-  get 'users/edit'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
+
+  resources :books, only:[:index, :show, :edit, :create]
+  resources :users, only:[:index, :show, :edit]
+
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

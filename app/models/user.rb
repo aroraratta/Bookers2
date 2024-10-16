3d55b52eadfe62d6312770383f71ca0fc8094959
aroraratta <177751775+aroraratta@users.noreply.github.com>
2024-10-16 14:54:12 +0000

    [Add]ユーザープロフィールを編集した際のフラッシュメッセージの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   has_one_attached :profile_image
-  
+
+  validates :name, presence: true
+
   has_many :books, dependent: :destroy
-  
+
   def get_profile_image(width, height)
     unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')

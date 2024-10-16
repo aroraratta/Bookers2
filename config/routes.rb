3e331bd8d81dabad6f57edcf9574060eaeba80a4
aroraratta <177751775+aroraratta@users.noreply.github.com>
2024-10-16 10:57:13 +0000

    [Add]詳細ページへDestroyボタンを追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get 'homes/about'
 
-  resources :books, only:[:index, :show, :edit, :update, :create]
+  resources :books, only:[:index, :show, :edit, :update, :create, :destroy]
   resources :users, only:[:index, :show, :edit]
 
   devise_for :users

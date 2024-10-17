6026f59ce568eaa834545e351973d32d0d92a6a9
aroraratta <177751775+aroraratta@users.noreply.github.com>
2024-10-17 07:29:34 +0000

    [Add]他ユーザーの編集制限コードの要約

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  
+  before_action :is_matching_login_user, only: [:edit, :update]
+  
   def index
     @users = User.all
   end
@@ -9,19 +12,10 @@ class UsersController < ApplicationController
   end
 
   def edit
-    user = User.find(params[:id])
-    unless user.id == current_user.id
-      redirect_to user_path(current_user.id)
-    end
     @user = User.find(params[:id])
   end
 
   def update
-    user = User.find(params[:id])
-    unless user.id == current_user.id
-      redirect_to user_path(current_user.id)
-    end
-    
     if @user.update(user_params)
       flash[:notice] = "You have updated user successfully."
       redirect_to user_path(@user.id)
@@ -34,6 +28,13 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+  
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to user_path(current_user.id)
+    end
   end
 
 end

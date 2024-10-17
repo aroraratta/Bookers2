36660cb93ebd501f45d446e082dccd29453d74d3
aroraratta <177751775+aroraratta@users.noreply.github.com>
2024-10-17 07:41:35 +0000

    [Add]他ユーザーの投稿の編集の制限

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,7 @@
 class BooksController < ApplicationController
+  
+  before_action :is_matching_login_user, only: [:edit, :update]
+  
   def index
     @book = Book.new
     @books = Book.all
@@ -46,5 +49,11 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :body)
   end
-
+  
+  def is_matching_login_user
+    book = Book.find(params[:id])
+    unless book.user.id == current_user.id
+      redirect_to books_path
+    end
+  end
 end
